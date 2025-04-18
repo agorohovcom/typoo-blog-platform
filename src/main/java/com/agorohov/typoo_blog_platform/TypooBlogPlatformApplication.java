965a63c31e520a2c139541a3350263dfb5e444fd
agorohov <aleksandr.gorohov@gmail.com>
2025-04-18 22:20:27 +0300

    init commit

---
+++ src/main/java/com/agorohov/typoo_blog_platform/TypooBlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.agorohov.typoo_blog_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypooBlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypooBlogPlatformApplication.class, args);
+	}
+
+}
