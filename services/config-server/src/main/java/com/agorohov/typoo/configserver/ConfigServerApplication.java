007290fd75812c2905283ca6791aa2ebde3fcba8
agorohov <aleksandr.gorohov@gmail.com>
2025-07-09 19:57:54 +0300

    Добавил опцию включения DotenvLoader с помощью @EnableDotenvLoader

--- services/config-server/src/main/java/com/agorohov/typoo/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/agorohov/typoo/configserver/ConfigServerApplication.java
@@ -1,11 +1,13 @@
 package com.agorohov.typoo.configserver;
 
+import com.agorohov.shared.utils.dotenvloader.EnableDotenvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDotenvLoader
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
