8cd2405369041a0efe7af9d4590dbd4b14aef3e8
agorohov <aleksandr.gorohov@gmail.com>
2025-07-13 00:30:43 +0300

    gateway-server добавлен

--- services/gateway-server/src/main/java/com/agorohov/typoo/gatewayserver/GatewayServerApplication.java
+++ services/gateway-server/src/main/java/com/agorohov/typoo/gatewayserver/GatewayServerApplication.java
@@ -3,10 +3,11 @@ package com.agorohov.typoo.gatewayserver;
 import com.agorohov.shared.utils.dotenvloader.EnableDotenvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableDotenvLoader
-//@EnableDiscoveryClient        // TODO это надо?!
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
