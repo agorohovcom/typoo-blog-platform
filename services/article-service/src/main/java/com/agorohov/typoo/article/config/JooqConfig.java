e3e181ec6090f652a51cfb9b0606d3654e4aee6e
agorohov <aleksandr.gorohov@gmail.com>
2025-11-11 19:41:28 +0300

    Удалил jooq и добавил лого

---
+++ services/article-service/src/main/java/com/agorohov/typoo/article/config/JooqConfig.java
@@ -0,0 +1,15 @@
+//package com.agorohov.typoo.article.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class JooqConfig {
+//
+//    @Bean
+//    public DSLContext dslContext(DataSource dataSource) {
+//        return DSL.using(dataSource, SQLDialect.POSTGRES);
+//    }
+//}
